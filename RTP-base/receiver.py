207786f016786b77b993490081879e90734c392e
homulily85 <nguyenkimtrungduc2005@gmail.com>
2025-04-06 06:43:15 +0700

    Create connection method for initialize connection

--- RTP-base/receiver.py
+++ RTP-base/receiver.py
@@ -1,28 +1,77 @@
 import argparse
 import socket
 
-from utils import PacketHeader, compute_checksum
+from utils import PacketHeader, compute_checksum, PACKET_TYPE
 
 
-def receiver(receiver_ip, receiver_port, window_size):
-    """TODO: Listen on socket and print received message to sys.stdout."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((receiver_ip, receiver_port))
-    while True:
-        # Receive packet; address includes both IP and port
-        pkt, address = s.recvfrom(2048)
+class UDPReceiver:
+    def __init__(self, receiver_ip, receiver_port, window_size):
+        """
+        Initialize the UDP receiver.
+        :param receiver_ip: The IP address of the host that receiver is running on.
+        :param receiver_port: The port number on which receiver is listening.
+        :param window_size: The maximum number of outstanding packets.
+        """
+        self.receiver_ip = receiver_ip
+        self.receiver_port = receiver_port
+        self.window_size = window_size
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.receiver_ip, self.receiver_port))
+        self.is_initialized = False
+        self.current_sender_ip = None
+        self.current_sender_port = None
+        self.expect_next = 1
 
-        # Extract header and payload
-        pkt_header = PacketHeader(pkt[:16])
-        msg = pkt[16 : 16 + pkt_header.length]
+    def start(self):
+        while True:
+            pkt, address = self.socket.recvfrom(1472)
+            # Extract header and payload
+            # The first 16 bytes are the header
+            pkt_header = PacketHeader(pkt[:16])
+            # The rest is the payload
+            msg = pkt[16: 16 + pkt_header.length]
+            # Check packet type
+            match pkt_header.type:
+                case PACKET_TYPE.START:
+                    if not self.is_initialized:
+                        self.is_initialized = True
+                        self.current_sender_ip = address[0]
+                        self.current_sender_port = address[1]
+                        print(f"Received START packet from {address[0]}:{address[1]}")
+                        print(
+                            f"Connected to sender {self.current_sender_ip}:{self.current_sender_port}")
 
-        # Verity checksum
+                        # Send ACK for START packet
+                        self._send_ack(self.expect_next)
+                        print("Sent ACK for START packet")
+                    elif (address[0] == self.current_sender_ip and
+                          address[1] == self.current_sender_port):
+                        print("Received duplicate START packet from the same sender")
+                        # Send ACK for duplicate START packet
+                        self._send_ack(self.expect_next)
+                    else:
+                        print(f"Received START packet from {address[0]}:{address[1]}")
+                        print("Ignored this START packet as receiver is already initialized")
+
+                case PACKET_TYPE.END:
+                    print("Received END packet")
+                    # Send ACK for END packet
+                    self._send_ack(0)  # Dummy seq_num
+                    break
+
+    def _send_ack(self, seq_num):
+        ack_packet = PacketHeader(type=PACKET_TYPE.ACK, seq_num=seq_num, length=0)
+        ack_packet.checksum = compute_checksum(ack_packet / b'')
+        self.socket.sendto(bytes(ack_packet), (self.current_sender_ip, self.current_sender_port))
+
+    @staticmethod
+    def _compare_checksum(msg, pkt_header):
         pkt_checksum = pkt_header.checksum
         pkt_header.checksum = 0
         computed_checksum = compute_checksum(pkt_header / msg)
         if pkt_checksum != computed_checksum:
-            print("checksums not match")
-        print(msg)
+            return False
+        return True
 
 
 def main():
@@ -38,7 +87,7 @@ def main():
     )
     args = parser.parse_args()
 
-    receiver(args.receiver_ip, args.receiver_port, args.window_size)
+    UDPReceiver(args.receiver_ip, args.receiver_port, args.window_size).start()
 
 
 if __name__ == "__main__":
--- RTP-base/sender.py
+++ RTP-base/sender.py
@@ -1,17 +1,89 @@
 import argparse
+import multiprocessing
 import socket
 import sys
 
-from utils import PacketHeader, compute_checksum
+from utils import PacketHeader, PACKET_TYPE, compute_checksum
 
 
-def sender(receiver_ip, receiver_port, window_size, message):
-    """TODO: Open socket and send message from sys.stdin."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    pkt_header = PacketHeader(type=2, seq_num=10, length=14)
-    pkt_header.checksum = compute_checksum(pkt_header / message)
-    pkt = pkt_header / message
-    s.sendto(bytes(pkt), (receiver_ip, receiver_port))
+class UDPSender:
+    def __init__(self, receiver_ip, receiver_port, window_size):
+        """
+        Initialize the UDP sender.
+        :param receiver_ip: The IP address of the receiver.
+        :param receiver_port: The port number on which the receiver is listening.
+        :param window_size: The maximum number of outstanding packets.
+        """
+        self.receiver_ip = receiver_ip
+        self.receiver_port = receiver_port
+        self.window_size = window_size
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.is_initialized = False
+        self.timeout = 5
+        self.base_seq_num = 0
+        self.next_seq_num = 0
+
+        self._send_start_packet()
+
+    def _send_start_packet(self):
+        """
+        Send a START packet to the receiver and wait for an ACK.
+        This is a blocking call.
+        The function will return when an ACK packet is received.
+        :return: None
+        """
+        start_packet = PacketHeader(type=PACKET_TYPE.START, seq_num=self.next_seq_num, length=0)
+        self.next_seq_num += 1
+        start_packet.checksum = compute_checksum(start_packet / b'')
+        while True:
+            self.socket.sendto(bytes(start_packet), (self.receiver_ip, self.receiver_port))
+            print("Sent START packet")
+            print("Waiting for ACK for START packet")
+            p = multiprocessing.Process(target=self._wait_for_ack, args=())
+            p.start()
+            p.join(self.timeout)
+            if p.is_alive():
+                print("Timeout waiting for ACK for START packet")
+                p.terminate()
+            else:
+                self.is_initialized = True
+                print("Received ACK for START packet")
+                break
+
+    def _send_end_packet(self):
+        """
+        Send an END packet to the receiver.
+        """
+        end_packet = PacketHeader(type=PACKET_TYPE.END, seq_num=self.next_seq_num, length=0)
+        end_packet.checksum = compute_checksum(end_packet / b'')
+        self.socket.sendto(bytes(end_packet), (self.receiver_ip, self.receiver_port))
+        print("Sent END packet")
+
+    def _wait_for_ack(self):
+        """
+        Wait for an ACK packet from the receiver.
+        This is a blocking call.
+        The function will return when an ACK packet is received.
+        :return: None
+        """
+        while True:
+            pkt, address = self.socket.recvfrom(1472)
+            # Extract header and payload
+            # The first 16 bytes are the header
+            pkt_header = PacketHeader(pkt[:16])
+            self.base_seq_num = pkt_header.seq_num
+            if pkt_header.type == PACKET_TYPE.ACK and self.base_seq_num == self.next_seq_num:
+                print(f"Received ACK for packet {self.base_seq_num - 1}")
+                break
+
+    @staticmethod
+    def _compare_checksum(msg, pkt_header):
+        pkt_checksum = pkt_header.checksum
+        pkt_header.checksum = 0
+        computed_checksum = compute_checksum(pkt_header / msg)
+        if pkt_checksum != computed_checksum:
+            return False
+        return True
 
 
 def main():
@@ -26,17 +98,16 @@ def main():
         "window_size", type=int, help="Maximum number of outstanding packets"
     )
     parser.add_argument(
-        "message", type=str, help="The message to be sent to the receiver", 
+        "message", type=str, help="The message to be sent to the receiver",
         nargs='?', default=None
     )
     args = parser.parse_args()
-    
-    # If message is not provided, read from stdin
+
     message = args.message
     if message is None:
         message = sys.stdin.read()
 
-    sender(args.receiver_ip, args.receiver_port, args.window_size, message)
+    sender = UDPSender(args.receiver_ip, args.receiver_port, args.window_size)
 
 
 if __name__ == "__main__":
